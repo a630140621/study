95359e3dee5a22aa8ceafd626fb5072dcecff0ca
a630140621 <630140621@qq.com>
2018-07-19 15:36:18 +0800

    完成数据处理python版本

--- handle-data/handle-data.py
+++ handle-data/handle-data.py
@@ -2,8 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-def main():
-    data_all = pd.read_csv('handle-data/0710.csv')
+'''
+file: 输入文件
+output: 输出文件
+'''
+
+
+def main(file, output):
+    data_all = pd.read_csv(file)
     # print(data_all['0.3um'])
     df = pd.DataFrame([data_all['0.3um'], data_all['0.5um'], data_all['1.0um'],
                        data_all['2.0um'], data_all['5.0um'], data_all['10.0um'], data_all['Sample Number']]).T
@@ -18,6 +24,7 @@ def main():
         if row['Sample Number'] == 1:
             number.append(i)
     number.append(length)
+    print(str(number))
     assert(len(number) == 19)
 
     # print(number)
@@ -29,7 +36,7 @@ def main():
         # 剔除异常值
         excluding_outliers_df = excluding_outliers(_df)
         # 保存
-        save(n + 1, excluding_outliers_df)
+        save(n + 1, excluding_outliers_df, output)
 
 
 def excluding_outliers(df):
@@ -39,7 +46,7 @@ def excluding_outliers(df):
     return df[(df - mean).abs() <= 3 * std]
 
 
-def save(n, df):
+def save(n, df, output):
     data = str(n) + '\n'
     _mean = df.mean().values  # 每一列的 平均数 list
     _max = df.max().values  # 最大值 list
@@ -52,9 +59,21 @@ def save(n, df):
     data += 'max, ' + ','.join(str(x) for x in _max) + '\n'
     data += 'min, ' + ','.join(str(x) for x in _min) + '\n'
     data += 'std, ' + ','.join(str(x) for x in _std) + '\n'
-    data += 'median, '+ ','.join(str(x) for x in _median) + '\n'
-    with open('handle-data/output_0710.csv', 'a') as f:
+    data += 'median, ' + ','.join(str(x) for x in _median) + '\n'
+    with open(output, 'a') as f:
         f.write(data)
 
 
-main()
+files = [('handle-data/0703.csv', 'handle-data/output_0703.csv'),
+         ('handle-data/0710.csv', 'handle-data/output_0710.csv'),
+         ('handle-data/0712.csv', 'handle-data/output_0712.csv'),
+         ('handle-data/0713.csv', 'handle-data/output_0713.csv'),
+         ('handle-data/0714.csv', 'handle-data/output_0714.csv'),
+         ('handle-data/0715.csv', 'handle-data/output_0715.csv'),
+         ('handle-data/0716.csv', 'handle-data/output_0716.csv'),
+         ('handle-data/0717.csv', 'handle-data/output_0717.csv'),
+         ('handle-data/0718.csv', 'handle-data/output_0718.csv')]
+
+# 执行
+for file in files:
+    main(file[0], file[1])
